1a0809b138e74612b9acd3fc7e112b50bf71f809
Dax Borde <daxborde+github@gmail.com>
2021-05-21 19:46:27 -0400

    use their app and index

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,21 +1,29 @@
-const Hello = (props) => {
+import React from 'react'
+
+const App = () => {
+  const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
+
   return (
     <div>
-      <p>Hello {props.name}, you are {props.age} years old.</p>
+      <h1>{course}</h1>
+      <p>
+        {part1} {exercises1}
+      </p>
+      <p>
+        {part2} {exercises2}
+      </p>
+      <p>
+        {part3} {exercises3}
+      </p>
+      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
     </div>
   )
 }
 
-const App = () => {
-  const name = 'Peter'
-  const age = 10
-
-  return (
-    <>
-      <h1>Greetings</h1>
-      <Hello name={name} age={age}/>
-    </>
-  )
-}
-
-export default App;
+export default App
