17eab79265e89396166d929cdf2b2bc069d2e3d5
Dax Borde <daxborde+github@gmail.com>
2021-05-24 21:00:15 -0400

    1.5

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -1,12 +1,12 @@
 import React from 'react'
 
 const Header = (props) => (
-  <h1>{props.course}</h1>
+  <h1>{props.course.name}</h1>
 )
 
 const Content = (props) => (
   <div >
-    {props.parts.map((x) => <Part part={x} key={x.name}/>)}
+    {props.course.parts.map((x) => <Part part={x} key={x.name}/>)}
   </div>
 )
 
@@ -17,29 +17,32 @@ const Part = (props) => (
 )
 
 const Total = (props) => (
-  <p>Number of exercises {props.parts.reduce((a, p) => a + p.exercises, 0)}</p>
+  <p>Number of exercises {props.course.parts.reduce((a, p) => a + p.exercises, 0)}</p>
 )
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const parts = [{
-    name: 'Fundamentals of React',
-    exercises: 10
-  },
-  {
-    name: 'Using props to pass data',
-    exercises: 7
-  },
-  {
-    name: 'State of a component',
-    exercises: 14
-  }]
+  const course = {
+    name: 'Half Stack application development',
+    parts: [{
+      name: 'Fundamentals of React',
+      exercises: 10
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7
+    },
+    {
+      name: 'State of a component',
+      exercises: 14
+    }]
+  }
+  
 
   return (
     <div>
       <Header course={course} />
-      <Content parts={parts} />
-      <Total parts={parts} />
+      <Content course={course} />
+      <Total course={course} />
     </div>
   )
 }
