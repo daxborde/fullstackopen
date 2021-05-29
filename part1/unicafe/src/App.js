fef30f9c488a214718ed62fe18cfa35bf180a696
Dax Borde <daxborde+github@gmail.com>
2021-05-28 22:36:14 -0400

    ex 1.6

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,7 +1,25 @@
-import React from 'react'
-const App = () => (
-  <div>
-    <p>Hello world</p> </div>
-)
+import React, { useState } from 'react'
+
+
+
+const App = () => {  
+  const [ goodc, setGoodC ] = useState(0);
+  const [ neutralc, setNeutralC ] = useState(0);
+  const [ badc, setBadC ] = useState(0);
+
+  return (
+    <>
+      <h1>Give Feedback</h1>
+      <button onClick={() => setGoodC(goodc + 1)}>good</button>
+      <button onClick={() => setNeutralC(neutralc + 1)}>neutral</button>
+      <button onClick={() => setBadC(badc + 1)}>bad</button>
+
+      <h1>Statistics</h1>
+      good {goodc} <br />
+      neutral {neutralc} <br />
+      bad {badc} <br />
+    </>
+  )
+}
 
 export default App
