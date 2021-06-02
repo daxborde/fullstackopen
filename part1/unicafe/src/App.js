17c23e8d08727963ed14f3a691e91edb6a32624a
Dax Borde <daxborde+github@gmail.com>
2021-06-01 20:29:08 -0400

    1.9 conditional render when no feedback given

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,6 +1,10 @@
 import React, { useState } from 'react'
 
 const Statistics = ({ mapping, history }) => {
+  if (history.length === 0) {
+    return <p>No feedback given</p>
+  }
+
   const countHistory = (history, key) => history.reduce((a, x) => a + (x === mapping[key] ? 1 : 0), 0);
 
   const numgood = countHistory(history, "good")
@@ -11,16 +15,14 @@ const Statistics = ({ mapping, history }) => {
   const positive = numgood / history.length
 
   return (
-    <>
-      <h1>Statistics</h1>
-      
+    <div>
       <p>good {numgood}</p>
       <p>neutral {numneutral}</p>
       <p>bad {numbad}</p>
       <p>all {history.length}</p>
       <p>average {average}</p>
       <p>positive %{positive * 100}</p>
-    </>
+    </div>
   )
 }
 
@@ -46,6 +48,7 @@ const App = () => {
         ))
       }
 
+      <h1>Statistics</h1>
       <Statistics mapping={mapping} history={history} />
     </>
   )
